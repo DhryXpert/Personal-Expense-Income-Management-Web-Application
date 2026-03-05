d6ff06962cd03c92407ebc3b5cf514dba490901d
DhryXpert <dhairyakhatri83@gmail.com>
2026-03-05 09:55:53 +0530

    Added Signup up page UI

--- frontend/experse-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/experse-tracker/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,70 @@
-import React from "react"; 
+import React, { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { Link, useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/input";
+import { validateEmail } from "../../utils/helper";
 
-const SignUp = () =>{
-    return(
-        <div>SignUp</div>
-    )
-}
+const SignUp = () => {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default SignUp
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  //Handle Sign Up Form Submit
+  const handleSignUp = async (e) => {};
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semiblack text-black">Create an Account</h3>
+        <p className="text-xs test-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+
+        <form onSubmit={handleSignUp}>
+
+        
+        
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({ target }) => setFullName(target.value)}
+              label="Full Name"
+              placeholder="John"
+              type="text"
+            />
+            <Input
+              value={email}
+              onChange={({ target }) => {
+                setEmail(target.value);
+                if (error) setError(null); // Clear error when typing
+              }}
+              label="Email Address"
+              placeholder="john@example.com"
+              type="text"
+            />
+
+            <div className="col-span-2">
+            <Input
+              value={password}
+              onChange={({ target }) => {
+                setPassword(target.value);
+                if (error) setError(null); // Clear error when typing
+              }}
+              label="Password"
+              placeholder="Min 8 Characters"
+              type="password"
+            />
+            </div>
+          </div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default SignUp;
