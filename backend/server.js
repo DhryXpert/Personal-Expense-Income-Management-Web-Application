2e637155c70ee1c3083733131253b646ebc90814
DhryXpert <dhairyakhatri83@gmail.com>
2026-03-08 16:23:13 +0530

    APIs for Login, Sign-Up, Get User Info, Profile Image Upload is completed

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const connectDB = require("./config/db")
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -12,12 +13,17 @@ app.use(
     origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  })
+  }),
 );
 
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/v1/auth", authRoutes);
+
+// Server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
