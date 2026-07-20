3da865a03998e335c8b2cf6cd357d39e4f63a225
DhryXpert <dhairyakhatri83@gmail.com>
2026-07-20 22:48:50 +0530

    Expense API added

---
+++ frontend/expense-tracker/src/components/Expense/ExpenseOverview.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {LuPlus} from "react-icons/lu";
+
+const ExpenseOverview = (transactions, onExpenseIncome) => {
+  return <div>ExpenseOverview</div>;
+};
+
+export default ExpenseOverview;
